ba0dd8104eac1951f10d8bdd51e99bdb60bd9ceb
Joseph Sanchez <josephsanchez@josephs-mbp.local>
2023-01-02 20:24:28 -0800

    service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './registerServiceWorker.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
+
+
+
+console.log(`${process.env.PUBLIC_URL}`)
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.register();
